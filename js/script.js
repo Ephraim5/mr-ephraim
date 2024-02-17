12b6758822d105690925a741b43a76d5f9044204
Ephraim5 <111990405+Ephraim5@users.noreply.github.com>
2024-02-17 22:34:05 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ var ul =document.querySelector("nav ul");
 hand.addEventListener("click",()=>{
     ul.classList.toggle("active")
 })
+var ld =document.getElementById('load');
+document.addEventListener("DOMContentLoaded",()=>{
+    setTimeout(function(){
+        ld.classList.add('active');
+    },2700)
+})
